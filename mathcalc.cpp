f63d9d3f2a49619c3eaf67f990bf067fd2509e04
IzumiSy <beetle-noise@gmx.com>
2015-08-14 21:43:11 +0900

    Added processing of brackets (in progress)

--- mathcalc.cpp
+++ mathcalc.cpp
@@ -35,12 +35,27 @@ void put_list_in_string(list<string> n)
     cout << endl;
 }
 
+int calcurate(list<string>::iterator begin, list<string>::iterator end)
+{
+    list<string>::iterator exp_begin, exp_end;
+    int r; 
+
+    begin++;
+    exp_begin = begin;
+    end--;
+    exp_end = end;
+    
+    return 10;
+    // return r;
+}
+
 int main(int argc, char *argv[]) 
 {
     string exp;
     list<string> exp_array;
     string::size_type current, prev,
-        pos_plus, pos_minus, pos_multi, pos_div;
+        pos_plus, pos_minus, pos_multi, pos_div,
+        pos_bracket_begin, pos_bracket_end;
     bool noexp;
 
     if (argc == 1) {
@@ -56,17 +71,21 @@ int main(int argc, char *argv[])
         pos_minus = exp.find("-", current);
         pos_multi = exp.find("*", current);
         pos_div = exp.find("/", current);
+        pos_bracket_begin = exp.find("(", current);
+        pos_bracket_end = exp.find(")", current);
         if (pos_plus == string::npos &&
             pos_minus == string::npos &&
             pos_multi == string::npos &&
-            pos_div == string::npos) {
+            pos_div == string::npos &&
+            pos_bracket_begin == string::npos &&
+            pos_bracket_end == string::npos) {
             noexp = true;
         }
 
         // number
         prev = current;
-        current = min(pos_plus, min(pos_minus,
-                  min(pos_multi, pos_div)));
+        current = min(pos_plus, min(pos_minus, min(pos_multi, 
+                  min(pos_div, min(pos_bracket_begin, pos_bracket_end)))));
         exp_array.push_back(exp.substr(prev, current - prev));
 
         // expression
@@ -78,14 +97,19 @@ int main(int argc, char *argv[])
     int right, left, let, exptype;
     string buf;
     list<string>::iterator it;
+    list<string>::iterator bracket_it;
+    list<string>::iterator bracket_start, bracket_end;
 
     put_list_in_string(exp_array);
 
     // process all multipulication and division at first
     // and after all of them are processed, it starts processing
     // the rest expressions such as addition and subtraction
-    bool _exists_mul_and_div, _exists_pls_and_mns;
+    bool _exists_mul_and_div;
+    bool _exists_pls_and_mns;
     bool _go_through = false;
+    bool in_bracket = false;
+    int bracket_value;
     while (1) {
         _exists_mul_and_div = false;
         _exists_pls_and_mns = false;
@@ -93,6 +117,33 @@ int main(int argc, char *argv[])
         it = exp_array.begin();
         while (it != exp_array.end()) {
             exptype = 0;
+            bracket_value = 0;
+
+            // Process brackets
+            if (*it == "(") {
+                if (in_bracket) {
+                    cout << "Error: bracket duplication" << endl;
+                    return 0;
+                }
+                in_bracket = true;
+                it--;
+                bracket_start = it;
+                it++;
+            } else if (*it == ")") {
+                if (!in_bracket) {
+                    cout << "Error: invalid bracket" << endl;
+                    return 0;
+                }
+                in_bracket = false;
+                it++; it++;
+                bracket_end = it;
+                bracket_it = bracket_start;
+                bracket_value = calcurate(bracket_start, bracket_end);
+                exp_array.erase(bracket_start, bracket_end);
+                exp_array.insert(it, _itos(bracket_value));
+            }
+
+            // Process expressions
             if (*it == "*") {
                 exptype = MUL;
                 _exists_mul_and_div = true;
